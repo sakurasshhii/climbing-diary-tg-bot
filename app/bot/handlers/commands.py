fa28cb30c6430ad40efeb3b879e78e41ecfcab10
Arina <skyliayandex.ru@gmail.com>
2026-04-20 21:09:05 +0300

    Заготовка под работу с текстовыми блоками

--- app/bot/handlers/commands.py
+++ app/bot/handlers/commands.py
@@ -1,10 +1,12 @@
 from aiogram import Bot, Router
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
+from app.lexic.ru import MAIN_MENU_MSG
 
 commands_router = Router()
 
 
+# /start
 @commands_router.message(CommandStart())
 async def process_start_command(
     message: Message,
@@ -15,4 +17,9 @@ async def process_start_command(
             message.from_user.id,
             message.from_user.username
         )
-    await message.answer(text="That's start message!")
+    await message.answer(text=MAIN_MENU_MSG['/start'])
+
+# /help
+@commands_router.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer(MAIN_MENU_MSG['/help'])
---
+++ app/lexic/ru.py
@@ -0,0 +1,8 @@
+MAIN_MENU: dict[str, str] = {
+    '/start': 'start',
+    '/help': 'help'
+}
+MAIN_MENU_MSG: dict[str, str] = {
+    '/start': 'will be soon...',
+    '/help': 'will  be soon...'
+}
